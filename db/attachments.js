916681772d22d30695a514c6cc8985d5e984055c
iamlaobie <iamlaobie@gmail.com>
2018-10-22 23:26:05 +0800

    rebuild

--- db/attachments.js
+++
@@ -1,3 +0,0 @@
-const db = require('../lib/db')
-
-exports.save = (data) => db.executeInsert('insert into attachments set ?', [data])
--- routes/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  users: require('./users'),
-  attachments: require('./attachments'),
-}
