6ab1dca212fd8294c8230af41db7eac81207e4b9
iamlaobie <iamlaobie@gmail.com>
2018-11-16 15:19:37 +0800

    optimise settings

--- api.js
+++ api.js
@@ -2,7 +2,6 @@ import Koa from 'koa'
 import koaBody from 'koa-body'
 import Router from 'koa-router'
 import { argv } from 'yargs'
-import adapters from './adapters'
 import route from './routes/index'
 import settings from './etc/settings'
 
@@ -11,8 +10,7 @@ const router = new Router()
 const app = new Koa()
 app.use(koaBody())
 
-// 绑定适配器的输入和输出
-adapters.bind(router, route.serve)
+router.get('/', route.serve)
 app.use(router.routes()).use(router.allowedMethods())
 
 const port = argv.port || settings.port || 8888
