2887633b7de2a6cc644052524fce9e122352913d
iamlaobie <iamlaobie@gmail.com>
2018-12-21 11:06:50 +0800

    use airbnb eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,45 +3,9 @@ module.exports = {
     "ecmaVersion": 6,
   },
   "parser": "babel-eslint",
-  "extends": ["eslint:recommended", "airbnb-base/legacy"],
+  "extends": ["eslint:recommended", "airbnb-base"],
   "env": {
     "es6": true,
     "node": true,
-  },
-  rules: {
-    "global-require": 0,
-    "func-names": 0,
-    "no-underscore-dangle": ['error', {allowAfterThis: true}],
-    "max-len": [1, 300],
-    "indent": [
-      2,
-      2
-    ],
-    "quotes": [
-      2,
-      "single"
-    ],
-    "linebreak-style": [
-      2,
-      "unix"
-    ],
-    "semi": [
-      2,
-      "never"
-    ],
-    "no-multi-spaces": [2],
-    "no-trailing-spaces": ["error"],
-    "space-infix-ops": ["error"],
-    "arrow-spacing": ["error"],
-    "space-before-blocks": ["error"],
-    "keyword-spacing": ["error"],
-    "comma-spacing": ["error"],
-    "no-restricted-syntax": 0,
-    "no-param-reassign": 0,
-    "no-await-in-loop": 0,
-    "camelcase": 0,
-    "no-mixed-operators": 0,
-    "comma-dangle": ["error", "always-multiline"],
-    "consistent-return": 0,
   }
 }
